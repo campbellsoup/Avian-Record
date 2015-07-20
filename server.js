1f10261a1c832811bf500e4734eb1781c934bf68
scaw76 <scawley@dmail.dixie.edu>
2015-07-20 10:19:24 -0600

    minor tweeks

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://mongodb.cs.dixie.edu/scawley');
 
 
 var app = express();
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 8080));
 
 app.set('views', './views');
 app.set('view engine', 'jade');
