40e1f819ca46544a3b425bf26094217cdefe45d1
scaw76 <scawley@dmail.dixie.edu>
2015-07-20 22:19:17 -0600

    Attached all input fields to db and each other .

--- models/app.data.js
+++ models/app.data.js
@@ -17,9 +17,28 @@ var mongoose = require('mongoose')
 
 var schema = new mongoose.Schema({
 	species: String,
+	subspecies: String,
+	hybrid: String,
+	band: String,
+	hatch: String,
+	gender: String,
+	mutation: String,
+	dad: String,
+	mom: String,
+	locate: String,
+	acquired: String,
+	origin: String,
+	owner: String,
 	pet: String,
-	location: String
+	imported: String,
+	chip: String,
+	dna: String,
+	available: String,
+	notes: String
 });
 var Inventory = mongoose.model('Inventory', schema);
 
 module.exports = Inventory;
+
+
+
