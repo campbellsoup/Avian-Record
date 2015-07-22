fbb198eb9bb6c216002d447572d39c1e57bd6050
campbellsoup <chad@reddirtcrew.com>
2015-07-21 21:37:53 -0600

    route controller model

--- routes/pairsNode.routes.js
+++ routes/pairsNode.routes.js
@@ -3,3 +3,20 @@ var express = require('express');
 var NodeController = require('../controllers/pairsNode.ctrl.js');
 
 var router = express.Router();
+
+//Create
+router.get('/pairs/new', 					NodeController.new);
+router.post('/pairs(.:format)?',	 		NodeController.create);
+
+//Read
+router.get('/pairs(.:format)?', 			NodeController.index);
+router.get('/pairs/(:id)(.:format)?', 		NodeController.show);
+
+//Update
+router.get('/pairs/:id/edit',	 			NodeController.edit);
+router.put('/pairs/(:id)(.:format)?',	 	NodeController.update);
+
+//Delete
+router.delete('/pairs/(:id)(.:format)?', 	NodeController.destroy);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var express = require('express'),
 	methodOverride = require('method-override');
 
 // access remote aviary database
-mongoose.connect('mongodb://mongodb.cs.dixie.edu/scawley');
-//mongoose.connect('mongodb://scaw76:collide76@ds047622.mongolab.com:47622/aviary');
+//mongoose.connect('mongodb://mongodb.cs.dixie.edu/scawley');
+mongoose.connect('mongodb://scaw76:collide76@ds047622.mongolab.com:47622/aviary');
 
 
 var app = express();
@@ -24,11 +24,12 @@ app.use('/', express.static('public'));
 // test a jade file  
 app.get('/', function (req,res)
 {
-	res.render('hello');
+	res.render('pairs/index');
 });
 
 // routes
 app.use('/', require('./routes/appNode.routes.js'));
+app.use('/', require('./routes/pairsNode.routes.js'));
 
 
 app.listen(app.get('port'), function()
